e6202b4dad44c19f9209a23626726e7a3b6a3f83
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2023-11-19 11:38:38 +0000

    Redesign

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -8,47 +8,44 @@
 
 
 
-    @section('navbar')
-        <li><a><span>Option 1</span></a></li>
-        <li><a><span>Option 2</span></a></li>
-        <li><a><span>Option 3</span></a></li>
-        <li><a><span>Option 4</span></a></li>
-    @endsection
+
 
 
 @section('content')
 <section class="container">
+                <img class="map" src="{{ asset('img/map.png') }}">
                 <section class="contactInfo">
                     <section>
-                        <h1>|&nbsp Contacts</h1>
+                        <h1>&nbsp Contacts</h1>
                         <ul class="info">
                             <li >
                                 &nbsp &nbsp &nbsp
                                 <img class="icon" src="{{ asset('img/email_icon.png') }}" alt="default user icon">
-                                <span>|&nbsp admin@gmail.com </span>
+                                <span>&nbsp admin@gmail.com </span>
                             </li>
                             <li >
                                 &nbsp &nbsp &nbsp
                                 <img class="icon" src="{{ asset('img/phone_icon.png') }}" alt="default user icon">
-                                <span>|&nbsp +351 224 119 040 </span>
+                                <span>&nbsp +351 224 119 040 </span>
                             </li>
                             <li >
                                 &nbsp &nbsp &nbsp
                                 <img class="icon" src="{{ asset('img/phone_icon.png') }}" alt="default user icon">
-                                <span>|&nbsp +351 934 119 040 </span>
+                                <span>&nbsp +351 934 119 040 </span>
                             </li>
                         </ul>
-                        <h1>|&nbsp Address</h1>
+                        </section>
+                        <section>
+                        <h1>&nbsp Address</h1>
                         <ul class="info">
                             <li >
-                                &nbsp &nbsp &nbsp |&nbsp 2791 Star Route Porto Portugal
+                                &nbsp R. Dr. Roberto Frias, 4200-465 Porto
+                            </li>
+                            <li >
+                                &nbsp  41.17870964921629, -8.596014526238392
                             </li>
                         </ul>
                     </section>
-                    <ul class="sci">
-                        <li><a href=""><i></i></a></li>
-                        <li><a href=""><i></i></a></li>
-                    </ul>
                 </section>
 </section>
 
