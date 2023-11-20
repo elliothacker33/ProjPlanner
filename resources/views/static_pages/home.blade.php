22d86fd46435ee913a57d33a83cdecdb7d719fc4
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2023-11-20 19:44:59 +0000

    Making it more responsive

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -12,17 +12,18 @@
     <section class="home-content">
         <section class="opening">
             <h1 class="shine">Welcome to ProjPlanner</h1>
-            <p>Your Ultimate Project Planning Platform</p>
         </section>
 
         <section class="home-center">
+            <p>Your Ultimate Project Planning Platform</p>
             <p>At ProjPlanner, we understand that every project is unique, and planning is the key to success. Whether
                 you're a seasoned project manager or embarking on your first venture, we've got you covered.</p>
+        </section>
+        <section class="get-started">
             <h2 class="shine">Get Started</h2>
-            <p>Get started today by signing up for free and take your first step towards turning your ideas into reality.
-            </p>
-            <a href="{{ url('/login') }}"><button>Sign In</button></a> <a
-                href="{{ url('/register') }}"><button>Register</button></a>
+            <p>Get started today by signing up for free and take your first step towards turning your ideas into reality.</p>
+            <a href="{{ url('/login') }}"><button>Sign In</button></a>
+            <a href="{{ url('/register') }}"><button>Register</button></a>
         </section>
     </section>
 @endsection
