b674ba6210919106f1eb2e5ee9ca530cdad4b9d6
ZeAntonioM <joseapmartins2003@gmail.com>
2023-11-20 17:28:52 +0000

    Finished simple home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,69 +1,73 @@
 <!DOCTYPE html>
 
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'ProjPlanner') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Styles -->
-        <link href="{{ url('css/milligram.min.css') }}" rel="stylesheet">
-        <link href="{{ url('css/app.css') }}" rel="stylesheet">
+    <title>{{ config('app.name', 'ProjPlanner') }}</title>
 
-        <script type="text/javascript">
-            // Fix for Firefox autofocus CSS bug
-            // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
-        </script>
-        <script type="text/javascript" src={{ url('js/app.js') }} defer>
-        </script>
+    <!-- Styles -->
+    <link href="{{ url('css/milligram.min.css') }}" rel="stylesheet">
+    <link href="{{ url('css/app.css') }}" rel="stylesheet">
 
-        @stack('styles')
-        <!-- Scripts -->
-        @stack('scripts')
+    <script type="text/javascript">
+        // Fix for Firefox autofocus CSS bug
+        // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
+    </script>
+    <script type="text/javascript" src={{ url('js/app.js') }} defer></script>
 
-    </head>
-    <body>
-        <header>
-            <section>
-                <h1>Project Planer</h1>
-                @if (Auth::check())
-                    <a class="user_icon" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
-                @else
-                    <a class="user_icon" href="{{ url('/login') }}"> <img class="icon" src="{{ asset('img/default_user.png') }}"
-                                                                          alt="default user icon"></a>
-                @endif
-            </section>
-            @if(View::hasSection('navbar'))
-                <nav>
-                    <ul>
-                        @yield('navbar')
-                    </ul>
-                </nav>
+    @stack('styles')
+    <!-- Scripts -->
+    @stack('scripts')
+
+</head>
+
+<body>
+    <header>
+        <section>
+            <h1>ProjPlanner</h1>
+            @if (Auth::check())
+                <a class="user_icon" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
+            @else
+                <a class="user_icon" href="{{ url('/login') }}"> <img class="icon"
+                        src="{{ asset('img/default_user.png') }}" alt="default user icon"></a>
             @endif
-
-        </header>
-        <main>
-
-            <section id="content">
-                @yield('content')
-            </section>
-
-        </main>
-        <footer>
-            <section>
+        </section>
+        @if (View::hasSection('navbar'))
+            <nav>
                 <ul>
-                    <li><a>FAQ</a></li>
-                    <li><a>About Us</a></li>
-                    <li><a href="{{ url('/contacts') }}">Contact Us</a></li>
+                    @yield('navbar')
                 </ul>
+            </nav>
+        @endif
 
-            </section>
-            <section><h6>@2023 Project Planer All rights reserve</h6></section>
-        </footer>
-    </body>
+    </header>
+    <main>
+
+        <section id="content">
+            @yield('content')
+        </section>
+
+    </main>
+    <footer>
+        <section>
+            <ul>
+                <li><a>FAQ</a></li>
+                <li><a>About Us</a></li>
+                <li><a href="{{ url('/contacts') }}">Contact Us</a></li>
+            </ul>
+
+        </section>
+        <section>
+            <h6>@2023 Project Planer All rights reserve</h6>
+        </section>
+    </footer>
+</body>
+
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,15 +10,19 @@
 
 @section('content')
     <section class="home-content">
-
-        <h1>Welcome to ProjPlanner</h1>
-        <p>Your Ultimate Project Planning Platform</p>
+        <section class="opening">
+            <h1 class="shine">Welcome to ProjPlanner</h1>
+            <p>Your Ultimate Project Planning Platform</p>
+        </section>
 
         <section class="home-center">
-            <h2>Empower Your Ideas, Realize Your Vision!</h2>
-            <p>At ProjPlanner, we understand that every project is unique, and planning is the key to success. Whether you're a seasoned project manager or embarking on your first venture, we've got you covered.</p>
-            <p>Get started today by signing up for free and take your first step towards turning your ideas into reality.</p>
-            <button class="signin-btn">Sign In</button> | <button class="register-btn">Register</button>
+            <p>At ProjPlanner, we understand that every project is unique, and planning is the key to success. Whether
+                you're a seasoned project manager or embarking on your first venture, we've got you covered.</p>
+            <h2 class="shine">Get Started</h2>
+            <p>Get started today by signing up for free and take your first step towards turning your ideas into reality.
+            </p>
+            <a href="{{ url('/login') }}"><button>Sign In</button></a> <a
+                href="{{ url('/register') }}"><button>Register</button></a>
         </section>
     </section>
 @endsection
