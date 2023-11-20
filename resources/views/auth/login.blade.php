d31ac0028664c27987f5202e458803a78def444c
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2023-11-20 01:04:20 +0000

    improving login form design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,42 +6,51 @@
         <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
     @endpush
 
-    <section class="login_form">
-<form method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
+    <section class="authentication">
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
+        <section class="formContainer">
+            <section class="header">
+                <p><span class="shine">Empower</span> Your Day, <span class="shine">Unleash</span> the work </p>
+                <h2>Login to Your Account</h2>
+            </section>
+
+
+        <form method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
+
+            <input id="email" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required autofocus>
+            @if ($errors->has('email'))
+                <span class="error">
           {{ $errors->first('email') }}
         </span>
-    @endif
+            @endif
 
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
+            <input id="password" type="password" name="password" placeholder="Password" required>
+            @if ($errors->has('password'))
+                <span class="error">
             {{ $errors->first('password') }}
         </span>
-    @endif
+            @endif
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
+            <label>
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+            </label>
 
-    <button type="submit">
-        Login
-    </button>
-    <a class="button button-outline" href="{{ route('register') }}">Register</a>
-    @if (session('success'))
-        <p class="success">
-            {{ session('success') }}
-        </p>
-    @endif
-</form>
-    <section class="container">
+            <button type="submit">
+                Login
+            </button>
 
+            @if (session('success'))
+                <p class="success">
+                    {{ session('success') }}
+                </p>
+            @endif
+        </form>
+        </section>
+        <section class="container">
+
+            <a class="button button-outline" href="{{ route('register') }}">Register</a>
+        </section>
     </section>
-</section>
+
 @endsection
