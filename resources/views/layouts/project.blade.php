b2ce56db117ce768e32170c922409313e78b1508
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2023-11-22 04:15:46 +0000

    Making the project page html and css

--- resources/views/layouts/project.blade.php
+++ resources/views/layouts/project.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/project.css') }}">
+@endpush
+
+
 @section('navbar')
     <li><a>Project Home</a></li>
     <li><a>Team</a></li>
--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -3,3 +3,68 @@
 @push('styles')
     <link rel="stylesheet" href="{{ asset('css/project.css') }}">
 @endpush
+@push('scripts')
+    <script type="text/javascript" src={{ url('js/app.js') }} defer></script>
+@endpush
+@section('content')
+<section class="projectPage">
+    <header>
+        <h1 class="title">Dummy project title</h1>
+        @if($project->is_archived) <span class="status archive"> Archive </span>
+        @else <span class="status open"> Open </span>
+        @endif
+        @can('edit',$project)
+            <select class="status">
+
+            </select>
+        @endcan
+    </header>
+    <section class="container">
+    <section class="primaryContainer">
+
+        <section class="description">
+            <p>dvbfkjdbfjnhbgdfjgb dfj bgkdjfbgkdbg
+            fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+            fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdfdvbfkjdbf jnhbgdfjgb dfj bgkdjfbgkdbg
+            fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+            fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdfdvbf kjdbfjnhbgdfjgb dfj bgkdjfbgkdbg
+            fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+            fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdf</p>
+            <p>dvbfkjdbfjnhbgdfjgb dfj bgkdjfbgkdbg
+                fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+                fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdfdvbfkjdbf jnhbgdfjgb dfj bgkdjfbgkdbg
+                fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+                fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdfdvbf kjdbfjnhbgdfjgb dfj bgkdjfbgkdbg
+                fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+                fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdf</p>
+            <p>dvbfkjdbfjnhbgdfjgb dfj bgkdjfbgkdbg
+                fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+                fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdfdvbfkjdbf jnhbgdfjgb dfj bgkdjfbgkdbg
+                fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+                fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdfdvbf kjdbfjnhbgdfjgb dfj bgkdjfbgkdbg
+                fshd fjhsdjf hskjdhfksdhfksdhf kjshdfjksd
+                fsjhdfkshdfkjs hdkfhskdjh fkjsdhfjkshdf</p>
+        </section>
+
+    </section>
+    <section class="sideContainer">
+        <section class="completionContainer">
+            <span class="completion">Completed Tasks 20/37</span>
+        </section>
+        <section class="deadlineContainer" >
+            <span>DeadLine: 20/9/2021</span>
+        </section>
+        <section class="teamContainer">
+            <h5>Team: </h5>
+            <ul class="team">
+                <li>DanielDos Santos</li>
+                <li>Francisco Cardoso</li>
+                <li>ZeAntonioM</li>
+                <li>Tomas Pereira</li>
+            </ul>
+            <a>See all</a>
+        </section>
+    </section>
+    </section>
+</section>
+@endsection
