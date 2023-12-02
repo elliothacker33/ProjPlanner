5f7f2abac8d8a46f6a8a18dc45a971880a1ef65c
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2023-12-02 21:03:50 +0000

    making user card

---
+++ resources/views/partials/userCard.blade.php
@@ -0,0 +1,10 @@
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/user.css') }}">
+@endpush
+<section class="userCard">
+    <img class="icon avatar" src="{{ asset('img/default_user.png') }}" alt="default user icon">
+    <section class="info">
+        <h3>{{$user->name}}</h3>
+        <h5>{{$user->email}}</h5>
+    </section>
+</section>
