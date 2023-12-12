c07423a01c8be3a1deecab9f02703f5e687492c9
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2023-12-12 01:35:03 +0000

    Start of team page html

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -1,37 +1,44 @@
 @extends('layouts.project')
 @push('styles')
     <link rel="stylesheet" href="{{ asset('css/team.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/partials/cards.css') }}">
 @endpush
 
+@push('scripts')
+    <script type="module" src="{{ asset('js/tasks.js') }}" defer></script>
+@endpush
+
+@section('content')
 <section class="team">
 
-    @can('update',[\App\Models\Project::class,$project])
-        <section class="addUserContainer">
-        <form method="POST" action="{{ route('addUser', ['project' => $project])  }}">
-            {{ csrf_field() }}
-            <input type="email" name="email" placeholder="User Email" required>
-
-            <button type="submit" > Add </button>
-        </form>
+    <section class="search">
+        <input type="search" placeholder="Search" aria-label="Search" id="search-bar" />
+        <a class="button" href="{{ route('createTask', ['project' => $project]) }}"> Add a Task  </a>
     </section>
-    @endcan
-    @if($errors->has('email'))
-        <span class="error">
-                        {{ $errors->first('email') }}
-                    </span>
-    @endif
-    <h2>Projects Member</h2>
-    <section class="teamContainer">
-        @foreach($team as $member)
-            <section class="member">
-                <p>{{$member->name}}</p>
-                <p>{{$member->email}}</p>
-            </section>
-        @endforeach
 
+    <section class="users-list">
+        <header>
+            <span> <i class="fa-solid fa-users"></i>  {{count($team)}} Members </span>
+
+        </header>
+        <section class="users">
+
+            <section class="user-item">
+                <section class="user">
+                @foreach($team as $user)
+                    @include('partials.userCard',['user'=>$user, 'size'=>'.median'])
+                    <span class="status coordinator"> Coordinator</span>
+                @endforeach
+                </section>
+                <section class="actions">
+                    <i class="fa-solid fa-user-minus"></i>
+                </section>
+            </section>
+        </section>
     </section>
 
 
 
 
 </section>
+@endsection
