a78d6634e89b4282864b7271aa556d8008f8d232
Tomas Pereira <up202108845@edu.fe.up.pt>
2023-12-17 17:05:09 +0000

    faq merged

--- resources/views/static/faq.blade.php
+++ resources/views/static/faq.blade.php
@@ -13,9 +13,9 @@
 
   <div class ="row"> 
 
-  <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 options p-5">
-
-      <div class="row header-options ">
+  <div class="col-2 col-sm-12 col-md-6 col-lg-4 col-xl-2 options p-5">
+      <div class="row">
+      <div class="col-12 header-options ">
         <h2>Faq Topics </h2>
       </div>
         <div class = "col-12 header-options">
