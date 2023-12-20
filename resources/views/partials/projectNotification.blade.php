1fbd85ad49795cc2045cab7f154ad6187e7d9829
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2023-12-20 20:05:57 +0000

    Notification Container and notification card designs

---
+++ resources/views/partials/projectNotification.blade.php
@@ -0,0 +1,14 @@
+<section class="notificationCard projectNotification">
+    <a>
+        <header>
+            <h4>Project Name</h4>
+            <i class="fa-solid fa-check"></i>
+        </header>
+        <section>
+            This is a random message just to test
+        </section>
+        <footer>
+            13-08-2023
+        </footer>
+    </a>
+</section>
