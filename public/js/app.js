cd0e8a9ae8f6bd5e84fb69521ebfe0b2580f4d1b
Daniel Ferreira <unidsferreira2003@gmail.com>
2023-12-21 20:39:57 +0000

    Solved conflicts that were in main

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,31 @@
-<<<<<<< HEAD
 import {
     projectHomePageRegex,
     projectTeamPageRegex,
     projectTaskPageRegex,
     adminUserPageRegex,
-    adminProjectPageRegex
+    adminProjectPageRegex,
+    projectHomePageRegex,
+    projectTeamPageRegex,
+    projectTaskPageRegex,
+    projectTagsPageRegex,
 } from "./const/regex.js";
-=======
-import {projectHomePageRegex, projectTeamPageRegex, projectTaskPageRegex, projectTagsPageRegex} from "./const/regex.js";
->>>>>>> main
 
 const currentPath = window.location.pathname;
 const projectHomePage = projectHomePageRegex.test(currentPath);
 const projectTeamPage = projectTeamPageRegex.test(currentPath);
 const projectTaskPage = projectTaskPageRegex.test(currentPath);
-<<<<<<< HEAD
 const adminUsersPage = adminUserPageRegex.test(currentPath);
 const projectAdminPage = adminProjectPageRegex.test(currentPath);
-=======
 const projectTagsPage = projectTagsPageRegex.test(currentPath);
->>>>>>> main
 
 console.log(adminUsersPage)
 if(projectTaskPage)document.querySelector('#projectTasks').classList.add('selected')
 else if (projectTeamPage)document.querySelector('#projectTeam').classList.add('selected')
 else if(projectHomePage) document.querySelector('#projectHome').classList.add('selected')
-<<<<<<< HEAD
 else if(adminUsersPage) document.querySelector('#adminUsersPage').classList.add('selected')
 else if( projectAdminPage) document.querySelector('#adminProjectsPage').classList.add('selected')
 
-=======
 else if(projectTagsPage) document.querySelector('#projectTags').classList.add('selected')
->>>>>>> main
 function buildFetchOptions(method, data) {
     const options = {
         method: method,
